caf7cd76fc5aaa3d844ae0b796cc900a2d18de7e
Rariolu <rcoyneyr82011@gmail.com>
2020-02-06 19:52:47 +0000

    Removing deprecated files

--- Assets/Scripts/GameUI/ContractViewportScript.cs
+++ Assets/Scripts/GameUI/ContractViewportScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ContractViewportScript : UIButton
 {
     public GameObject ContractButtonUI;
+
     // Start is called before the first frame update
     protected override void Start()
     {
@@ -15,7 +16,6 @@ public class ContractViewportScript : UIButton
 
     void ContractButton_Click(UIButton sender)
     {
-        //
         Debug.Log("Contract button clicked");
         if (Util.IsPaused)
         {
@@ -26,22 +26,16 @@ public class ContractViewportScript : UIButton
             Pause();
         }
     }
-    /*void Update()
-    {
-        base.Start();
-        Click -= ContractButton_Click;
-    }*/
+
     void Resume()
     {
         ContractButtonUI.SetActive(false);
-        //Time.timeScale = 1f;
         Util.IsPaused = false;
     }
 
     void Pause()
     {
         ContractButtonUI.SetActive(true);
-        //Time.timeScale = 0f;
         Util.IsPaused = true;
     }
 }
