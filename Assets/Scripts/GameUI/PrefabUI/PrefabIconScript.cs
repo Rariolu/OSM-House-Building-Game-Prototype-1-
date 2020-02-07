4d0ef58c605204cc60c4cfd7900096b541ab0686
Coyne, Robbie <RCOYNE200@caledonian.ac.uk>
2020-02-07 12:27:01 +0000

    PrefabIconScript created, monobehaviour intended to be put on pre-made UI buttons.

---
+++ Assets/Scripts/GameUI/PrefabUI/PrefabIconScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void PrefabIconScriptClick(PrefabIconScript sender);
+
+public class PrefabIconScript : MonoBehaviour
+{
+    Prefab prefab;
+    public Prefab Prefab
+    {
+        get
+        {
+            return prefab;
+        }
+        set
+        {
+            prefab = value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
