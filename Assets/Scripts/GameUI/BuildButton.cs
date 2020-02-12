136a595beddb09cbb8a32c120515d34bfdf28be2
ebrown212 <45566901+ebrown212@users.noreply.github.com>
2020-02-12 21:46:33 +0000

    Change in the UI for Build buttons

---
+++ Assets/Scripts/GameUI/BuildButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildButton : MonoBehaviour
+{
+
+    public bool IsBuildActive;
+    public GameObject btnWallPrefabs;
+    public GameObject btnFloorPrefabs;
+    public GameObject btnRoofPrefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnWallPrefabs.SetActive(false);
+        btnFloorPrefabs.SetActive(false);
+        btnRoofPrefabs.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void BuildActive()
+    {
+        {
+            btnWallPrefabs.SetActive(true);
+        }
+        {
+            btnFloorPrefabs.SetActive(true);
+        }
+        {
+            btnRoofPrefabs.SetActive(true);
+        }
+    }
+
+}
