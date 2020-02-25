f20b3126337a751f3e5eaa4777bb65acb63e9cc6
ebrown212 <45566901+ebrown212@users.noreply.github.com>
2020-02-25 13:48:46 +0000

    CamSwapIntroduced

---
+++ Assets/Scripts/Camera/CamSwitch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamSwitch : MonoBehaviour
+{
+    public GameObject Cam1;
+    public GameObject Cam2;
+
+    void Start()
+    {
+        Cam1.SetActive(true);
+        Cam2.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("1Key"))
+        {
+            Cam1.SetActive(true);
+            Cam2.SetActive(false);
+        }
+        if (Input.GetButtonDown("2Key"))
+        {
+            Cam1.SetActive(false);
+            Cam2.SetActive(true);
+        }
+    }
+}
