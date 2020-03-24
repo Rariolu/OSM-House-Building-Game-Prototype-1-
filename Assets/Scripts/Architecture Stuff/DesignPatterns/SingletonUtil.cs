54493180da5b773f3826dc441659636fcb6b7274
Rariolu <rcoyneyr82011@gmail.com>
2020-03-24 04:14:02 +0000

    Comments

--- Assets/Scripts/Architecture Stuff/DesignPatterns/SingletonUtil.cs
+++ Assets/Scripts/Architecture Stuff/DesignPatterns/SingletonUtil.cs
@@ -6,8 +6,18 @@ using System.Threading.Tasks;
 
 public static class SingletonUtil
 {
+    /// <summary>
+    /// A dictionary that stores the only possible instance of each type.
+    /// </summary>
     static Dictionary<Type, object> instances = new Dictionary<Type, object>();
 
+    /// <summary>
+    /// Add (or replace) the given type and value to the
+    /// dictionary of stored instances so that the instance
+    /// can be retrieved.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="inst"></param>
     public static void SetInstance<T>(T inst)
     {
         Type type = typeof(T);
@@ -21,6 +31,13 @@ public static class SingletonUtil
         }
     }
 
+    /// <summary>
+    /// Check if the dictionary stores a value of the 
+    /// requested type, and output the value if found.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="inst"></param>
+    /// <returns></returns>
     public static bool InstanceAvailable<T>(out T inst)
     {
         Type type = typeof(T);
