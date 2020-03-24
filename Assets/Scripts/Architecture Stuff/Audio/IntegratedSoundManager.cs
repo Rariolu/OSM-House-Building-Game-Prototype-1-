9394158b1620d1fe48729e7e426670309b0f33af
Rariolu <rcoyneyr82011@gmail.com>
2020-03-24 09:35:41 +0000

    Separating audio management into separate files.

---
+++ Assets/Scripts/Architecture Stuff/Audio/IntegratedSoundManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class IntegratedSoundManager
+{
+    static AudioMixer mixer;
+    static Dictionary<string, Sound> soundDict = new Dictionary<string, Sound>();
+
+    public static void AddSound(Sound s)
+    {
+        if (!soundDict.ContainsKey(s.name.ToString()))
+        {
+            soundDict.Add(s.name.ToString(), s);
+        }
+        else
+        {
+            Logger.Log("\"{0}\" already added to dictionary.", s.name.ToString());
+        }
+    }
+
+    static UpdateableAudioSource CreateAudioSource(string name = "audio")
+    {
+        GameObject go = new GameObject();
+        go.name = name;
+        UpdateableAudioSource audioSource = go.AddComponent<UpdateableAudioSource>();
+        return audioSource;
+    }
+
+    public static IEnumerator PlaySound(SOUNDNAME soundName)
+    {
+        yield return PlaySound(soundName.ToString());
+    }
+
+    public static IEnumerator PlaySound(string name)
+    {
+        if (soundDict.ContainsKey(name))
+        {
+            Sound sound = soundDict[name];
+            UpdateableAudioSource audioSource = CreateAudioSource(name);
+            yield return audioSource.Play(sound, mixer);
+        }
+        else
+        {
+            Logger.Log("soundDict doesn't contain \"{0}\".", name);
+        }
+    }
+
+    public static void PlaySoundAsync(SOUNDNAME soundName)
+    {
+        PlaySoundAsync(soundName.ToString());
+    }
+
+    public static void PlaySoundAsync(string name)
+    {
+        if (soundDict.ContainsKey(name))
+        {
+            Sound sound = soundDict[name];
+            UpdateableAudioSource audioSource = CreateAudioSource(name);
+            audioSource.PlayAsync(sound, mixer);
+        }
+        else
+        {
+            Logger.Log("soundDict doesn't contain \"{0}\".", name);
+        }
+    }
+
+    public static void SetMixer(AudioMixer _mixer)
+    {
+        mixer = _mixer;
+    }
+}
---
+++ Assets/Scripts/Architecture Stuff/Audio/Sound.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+    public bool loop = false;
+    public float volume = 1;
+    public SoundType type;
+}
