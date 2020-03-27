04c496f9d8e083acf298464e3fdd9a0129477606
Rariolu <rcoyneyr82011@gmail.com>
2020-03-27 08:17:18 +0000

    End condition applied to "Sell house" button.

---
+++ Assets/Scripts/GameUI/SellHouseButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SellHouseButton : UIButton
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        Click += SellHouseButton_Click;
+    }
+
+    void SellHouseButton_Click(UIButton sender)
+    {
+        IntegratedSoundManager.PlaySoundAsync(SOUNDNAME.SELL_HOUSE);
+        Util.SetExitState(EndConditionUtil.Pass());
+    }
+}
