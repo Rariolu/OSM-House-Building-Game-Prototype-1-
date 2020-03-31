8392706429a0349b10821bb9fbc2495fbdc925fd
Rariolu <rcoyneyr82011@gmail.com>
2020-03-31 17:45:42 +0100

    Script switches between contract selection states.

--- Assets/Scripts/ContractStuff/ContractSelectionScript.cs
+++ Assets/Scripts/ContractStuff/ContractSelectionScript.cs
@@ -2,12 +2,74 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class ContractSelectionScript : MonoBehaviour
+[RequireComponent(typeof(Image))]
+public class ContractSelectionScript : MultitonScript<ContractSelectionScript,int>
 {
     public Text lblContractName;
     public UIButton btnStart;
     public UIButton btnBrochure;
+    public UIButton btnBack;
+    public UIButton btnForward;
+    public Image pbClosedContract;
+    public Image pbOpenContract;
+    public Image pbOpenMeta;
     Contract contract;
+    int id;
+    public int ID
+    {
+        get
+        {
+            return id;
+        }
+    }
+    static int count = 0;
+    enum ContractSelectionState
+    {
+        CLOSED,
+        OPEN_MAIN,
+        OPEN_META
+    }
+    ContractSelectionState state = ContractSelectionState.CLOSED;
+    ContractSelectionState State
+    {
+        get
+        {
+            return state;
+        }
+        set
+        {
+            state = value;
+            if (pbOpenContract != null)
+            {
+                pbOpenContract.gameObject.SetActive(state == ContractSelectionState.OPEN_MAIN);
+            }
+
+            if (pbOpenMeta != null)
+            {
+                pbOpenMeta.gameObject.SetActive(state == ContractSelectionState.OPEN_META);
+            }
+
+            if (pbClosedContract != null)
+            {
+                pbClosedContract.gameObject.SetActive(state == ContractSelectionState.CLOSED);
+            }
+
+            if (btnForward != null)
+            {
+                btnForward.gameObject.SetActive(state != ContractSelectionState.CLOSED);
+            }
+
+            if (btnBack != null)
+            {
+                btnBack.gameObject.SetActive(state != ContractSelectionState.CLOSED);
+            }
+        }
+    }
+    private void Awake()
+    {
+        SetInstance(id = count++, this);
+    }
+
     public void SetActive(bool active)
     {
         gameObject.SetActive(active);
@@ -23,6 +85,7 @@ public class ContractSelectionScript : MonoBehaviour
     
     void Start()
     {
+        State = ContractSelectionState.CLOSED;
         if (btnStart != null)
         {
             btnStart.Click += StartContract;
@@ -33,22 +96,68 @@ public class ContractSelectionScript : MonoBehaviour
         }
         if (btnBrochure != null)
         {
-            btnBrochure.Click += (sender) => { Logger.Log("btnBrochure clicked."); };
+            btnBrochure.Click += btnBrochure_Click;
+            //btnBrochure.Click += (sender) => { Logger.Log("btnBrochure clicked."); };
         }
         else
         {
             Logger.Log("btnBrochure was null.");
         }
+
+        if (btnForward != null)
+        {
+            btnForward.Click += btnForward_Click;
+        }
+
+        if (btnBack != null)
+        {
+            btnBack.Click += btnBack_Click;
+        }
     }
+
+    void btnBrochure_Click(UIButton sender)
+    {
+        State = ContractSelectionState.OPEN_MAIN;
+    }
+
+    void btnForward_Click(UIButton sender)
+    {
+        State = State != ContractSelectionState.OPEN_META ? (ContractSelectionState)((int)State + 1) : ContractSelectionState.CLOSED;
+    }
+
+    void btnBack_Click(UIButton sender)
+    {
+        State = State != ContractSelectionState.CLOSED ? (ContractSelectionState)((int)State - 1) : ContractSelectionState.OPEN_META;
+    }
+
     void StartContract(UIButton sender)
     {
         if (contract != null)
         {
+            Clear();
             Util.SetContract(contract);
         }
         else
         {
             Logger.Log("Contract was null.");
         }
+    }
+
+    void OpenContract()
+    {
+        //Close the other contracts
+        foreach(ContractSelectionScript contractSelection in Values)
+        {
+            if (contractSelection.ID != ID)
+            {
+                contractSelection.CloseContract();
+            }
+        }
+
+    }
+
+    public void CloseContract()
+    {
+        State = ContractSelectionState.CLOSED;
     }
 }
