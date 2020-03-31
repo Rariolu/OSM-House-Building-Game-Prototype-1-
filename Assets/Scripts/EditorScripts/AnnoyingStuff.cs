884fd6c3d45f11c35d92003b3a7d9fe3901449af
Rariolu <rcoyneyr82011@gmail.com>
2020-03-31 10:24:42 +0100

    Editor script created

---
+++ Assets/Scripts/EditorScripts/AnnoyingStuff.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+#if UNITY_EDITOR
+
+public class AnnoyingStuff : ScriptableObject
+{
+    [MenuItem("Tools/Annoying Stuff/Delete Post-Processing folder")]
+    static void DoIt()
+    {
+        const string dir = "Library\\PackageCache\\com.unity.postprocessing@2.0.3-preview";
+        if (Directory.Exists(dir))
+        {
+            string fullPath = Path.GetFullPath(dir);
+            if (EditorUtility.DisplayDialog("Post-Processing", "Delete annoying post-processing folder. "+fullPath, "Yes", "No"))
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(fullPath);
+                directoryInfo.Delete(true);
+                Logger.Log("\"{0}\" successfully deleted.",fullPath);
+            }
+        }
+        else
+        {
+            Logger.Log(string.Format("\"{0}\" doesn't exist...", dir), LogType.Warning);
+        }
+      
+    }
+}
+
+#endif
