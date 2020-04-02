1de797c06dda2da6d694a5278d34af9bbbff070a
ebrown212 <45566901+ebrown212@users.noreply.github.com>
2020-04-02 20:18:30 +0100

    Pause Menu Update, Enivron 2 Implement

---
+++ Assets/Scripts/ContractStuff/ContractUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContractUI : MonoBehaviour
+{
+    public string Menu;
+    public bool IsContractActive;
+    public GameObject ContractMenuCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ContractMenuCanvas.SetActive(false);
+
+    }
+
+    bool contractActive = false;
+    public void ContractActive()
+    {
+        contractActive = !contractActive;
+        ContractMenuCanvas.SetActive(contractActive);
+    }
+    public void Continue()
+    {
+        IsContractActive = false;
+    }
+}
