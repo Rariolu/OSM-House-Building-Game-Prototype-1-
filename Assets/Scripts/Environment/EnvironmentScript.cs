86586a21021e37957132bee22e7d2fd257a5c78e
Rariolu <rcoyneyr82011@gmail.com>
2020-04-02 18:53:04 +0100

    Environment script created

---
+++ Assets/Scripts/Environment/EnvironmentScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnvironmentScript : MultitonScript<EnvironmentScript,FINISHED_CONSTRUCTION>
+{
+    public FINISHED_CONSTRUCTION contract;
+    private void Awake()
+    {
+        SetInstance(contract, this);
+    }
+
+    public void SetActive(bool active)
+    {
+        gameObject.SetActive(true);
+    }
+}
