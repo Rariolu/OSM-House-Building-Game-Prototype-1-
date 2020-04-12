ca3464812187c955b23302319cb5e2f5f16e4b71
Rariolu <rcoyneyr82011@gmail.com>
2020-04-12 22:02:00 +0100

    Subtask panel added

---
+++ Assets/Scripts/GameUI/Subtasks/SubtaskPanel.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SubtaskPanel : MonoBehaviour
+{
+    public Text lblBudget;
+
+    int budget;
+    int Budget
+    {
+        get
+        {
+            return budget;
+        }
+        set
+        {
+            budget = value;
+            if (lblBudget != null)
+            {
+                lblBudget.text = "£{0}".FormatText(budget);
+            }
+            else
+            {
+                Logger.Log("lblBudget is null.");
+            }
+        }
+    }
+
+
+
+    private void Awake()
+    {
+        SingletonUtil.SetInstance(this);
+    }
+    // Use this for initialization
+    void Start()
+    {
+        ConstructionUtil constructionUtil;
+        if (SingletonUtil.InstanceAvailable(out constructionUtil))
+        {
+            SetContract(constructionUtil.Contract);
+        }
+    }
+
+    void SetContract(Contract contract)
+    {
+        Budget = contract.budget;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
