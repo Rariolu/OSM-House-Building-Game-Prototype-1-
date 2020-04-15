a9662044434c952e1fd477d2aa2d3054414a83c6
Rariolu <rcoyneyr82011@gmail.com>
2020-04-15 05:28:29 +0100

    Intersection Launcher added

---
+++ Assets/Scripts/Fixings_MiniGames/IntersectionLauncher.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntersectionLauncher : MonoBehaviour
+{
+    //public SnapPointLauncher snapPoints;
+    private void Awake()
+    {
+        Intersection intersection = new Intersection(gameObject);
+    }
+}
