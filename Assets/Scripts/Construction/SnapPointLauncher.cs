560a61d253df327b2616c09e3ed7342409cf78fc
Rariolu <rcoyneyr82011@gmail.com>
2020-04-15 07:41:48 +0100

    Adding trigger property

--- Assets/Scripts/Construction/SnapPointLauncher.cs
+++ Assets/Scripts/Construction/SnapPointLauncher.cs
@@ -4,6 +4,14 @@ using System.Collections;
 public class SnapPointLauncher : MonoBehaviour
 {
     public SNAP_POINT_TYPE snapType;
+    SnapPointTrigger trigger;
+    public SnapPointTrigger Trigger
+    {
+        get
+        {
+            return trigger ?? (trigger = GetComponent<SnapPointTrigger>());
+        }
+    }
     private void Awake()
     {
         SnapPoint snapPoint = new SnapPoint(snapType, gameObject);
