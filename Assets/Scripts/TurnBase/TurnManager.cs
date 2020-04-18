c4ab6ac4ff34469fa5aa21fde42095e8a032b686
Rariolu <rcoyneyr82011@gmail.com>
2020-04-18 02:29:58 +0100

    TurnBase added

---
+++ Assets/Scripts/TurnBase/TurnManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnManager : MonoBehaviour
+{
+    public uint turnsPerMonth = 10;
+    int currentTurns;
+    private void Awake()
+    {
+        SingletonUtil.SetInstance(this);
+    }
+    public void IncrementTurns()
+    {
+        currentTurns++;
+        if (currentTurns >= turnsPerMonth)
+        {
+            currentTurns = 0;
+            ConstructionUtil util;
+            if (SingletonUtil.InstanceAvailable(out util))
+            {
+                util.IncrementDaysPassed();
+                Logger.Log("Months: {0};", util.DaysPassed);
+            }
+        }
+        
+    }
+}
