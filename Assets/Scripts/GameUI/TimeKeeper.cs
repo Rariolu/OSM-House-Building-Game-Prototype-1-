de43e9dc27ac84a8bcd073a556f7418b9dab0c48
Rariolu <rcoyneyr82011@gmail.com>
2020-04-18 05:16:10 +0100

    Month counter added

---
+++ Assets/Scripts/GameUI/TimeKeeper.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Text))]
+public class TimeKeeper : MonoBehaviour
+{
+    Text label;
+    Text Label
+    {
+        get
+        {
+            return label ?? (label = GetComponent<Text>());
+        }
+    }
+    private void Awake()
+    {
+        SingletonUtil.SetInstance(this);
+    }
+    
+    void Start()
+    {
+        ConstructionUtil util;
+        if (SingletonUtil.InstanceAvailable(out util))
+        {
+            util.TimePassed += TimePassed;
+        }
+    }
+
+    void TimePassed(uint time)
+    {
+        Label.text = "Month: {0}".FormatText(time);
+    }
+}
