17c11a7602f650e86066a38192d0f84461cbc6aa
Rariolu <rcoyneyr82011@gmail.com>
2020-04-19 05:38:16 +0100

    Generic activation button added.

---
+++ Assets/Scripts/GameUI/Buttons/ActiveToggleButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActiveToggleButton : UIButton
+{
+    public GameObject[] gameObjects;
+    public bool initiallyActive = false;
+    bool objectsActive;
+    // Use this for initialization
+    protected override void Start()
+    {
+        base.Start();
+        Click += ActiveToggleButton_Click;
+        objectsActive = initiallyActive;
+        SetActive(objectsActive);
+    }
+
+    void ActiveToggleButton_Click(UIButton sender)
+    {
+        objectsActive = !objectsActive;
+        SetActive(objectsActive);
+    }
+
+    void SetActive(bool active)
+    {
+        foreach(GameObject obj in gameObjects)
+        {
+            obj.SetActive(active);
+        }
+    }
+}
