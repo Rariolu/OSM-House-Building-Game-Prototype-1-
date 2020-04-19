06ff8bcc3de56e4562ce9fe42cd08ff01f59c00a
Rariolu <rcoyneyr82011@gmail.com>
2020-04-19 05:23:05 +0100

    BuildButton replaced.

---
+++ Assets/Scripts/GameUI/Buttons/BuildUIButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildUIButton : UIButton
+{
+    bool buildActive = false;
+    public GameObject btnWallPrefabs;
+    public GameObject btnFloorPrefabs;
+    public GameObject btnRoofPrefabs;
+    protected override void Start()
+    {
+        base.Start();
+        Click += BuildUIButton_Click;
+        btnWallPrefabs.SetActive(false);
+        btnFloorPrefabs.SetActive(false);
+        btnRoofPrefabs.SetActive(false);
+    }
+    
+    void BuildUIButton_Click(UIButton sender)
+    {
+        buildActive = !buildActive;
+        btnWallPrefabs.SetActive(buildActive);
+        btnFloorPrefabs.SetActive(buildActive);
+        btnRoofPrefabs.SetActive(buildActive);
+    }
+}
