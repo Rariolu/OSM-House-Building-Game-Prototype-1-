412dc4fe9929694f9c42c3507127e2fb6c809f40
Rariolu <rcoyneyr82011@gmail.com>
2020-04-19 06:13:46 +0100

    Added new camera switch button.

---
+++ Assets/Scripts/GameUI/Buttons/CameraSwitchButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSwitchButton : UIButton
+{
+    public GameObject mainCamera;
+    public GameObject topCamera;
+    bool mainCameraActive = true;
+    protected override void Start()
+    {
+        base.Start();
+        Click += CameraSwitchButton_Click;
+        SetMainCameraActive(true);
+    }
+
+    void CameraSwitchButton_Click(UIButton sender)
+    {
+        mainCameraActive = !mainCameraActive;
+        SetMainCameraActive(mainCameraActive);
+    }
+
+    void SetMainCameraActive(bool active)
+    {
+        mainCamera.SetActive(active);
+        topCamera.SetActive(!active);
+    }
+}
