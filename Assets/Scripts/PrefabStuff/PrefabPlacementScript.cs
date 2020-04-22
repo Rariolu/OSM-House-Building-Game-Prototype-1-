229e65664cb7490c33d531a8c1dbf2cbe2930658
Rariolu <rcoyneyr82011@gmail.com>
2020-04-22 11:45:01 +0100

    vib

--- Assets/Scripts/PrefabStuff/PrefabPlacementScript.cs
+++ Assets/Scripts/PrefabStuff/PrefabPlacementScript.cs
@@ -80,9 +80,28 @@ public class PrefabPlacementScript : MonoBehaviour
         }
     }
     const float delay = 1f;
+    const float vibRange = 1f;
+    float dur = 0.1f;
     IEnumerator Destruct()
     {
-        Animator.SetBool("Exterior_Wall_Des", true);
+        Vector3 ogPos = transform.position;
+        float minX = ogPos.x - vibRange;
+        float maxX = ogPos.x + vibRange;
+        float minY = ogPos.y - vibRange;
+        float maxY = ogPos.y + vibRange;
+        float minZ = ogPos.z - vibRange;
+        float maxZ = ogPos.z + vibRange;
+        float t = 0;
+        while (t < delay)
+        {
+            t += Time.deltaTime;
+            float x = ogPos.x + Mathf.PerlinNoise(ogPos.x, t*100f);
+            float y = ogPos.y + Mathf.PerlinNoise(ogPos.y, t*100f);
+            float z = ogPos.z + Mathf.PerlinNoise(ogPos.z, t*100f);
+            Logger.Log("X: {0}; Y: {1}; Z: {2};", x, y, z);
+            transform.position = new Vector3(x, y, z);
+        }
+        //Animator.SetBool("Exterior_Wall_Des", true);
         yield return new WaitForSeconds(delay);
         IntegratedSoundManager.PlaySoundAsync(SOUNDNAME.DESTRUCTION_0);
         Destroy(gameObject);
